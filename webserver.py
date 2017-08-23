29059e205becaba6ffa2d1ab42aa722f4b0ddbf2
Peretz <peretzman@gmail.com>
2017-08-23 11:26:06 -0500

    Task 3: Can create a new restaurant which reflects in the database.

--- webserver.py
+++ webserver.py
@@ -29,6 +29,22 @@ class webServerHandler(BaseHTTPRequestHandler):
                     output += "<a href=/delete>Delete</a><br>"
                     output += "<br></p>"
 
+                output += "<p><a href=/newrestaurant>Make a New Restaurant Here.</a></p>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+
+            if self.path.endswith("/newrestaurant"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Make a New Restaurant</h1>"
+
+                output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants'><input name="message" type="text" ><input type="submit" value="Create"> </form>'''
+                
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
@@ -46,12 +62,22 @@ class webServerHandler(BaseHTTPRequestHandler):
             ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
+                newRestaurantName = fields.get('message')
+                newRestaurant = Restaurant(name = newRestaurantName[0])
+                session.add(newRestaurant)
+                session.commit()
+
             output = ""
             output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            
+            restaurants = session.query(Restaurant).all()
+            for restaurant in restaurants:
+                output += "<p>" + restaurant.name + "<br>"
+                output += "<a href=/edit>Edit</a><br>"
+                output += "<a href=/delete>Delete</a><br>"
+                output += "<br></p>"
+
+            output += "<p><a href=/newrestaurant>Make a New Restaurant Here.</a></p>"
             output += "</body></html>"
             self.wfile.write(output)
             print output
