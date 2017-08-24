b3c3e17d002e6aea4ffa89f61f2e3963f02bfc20
Peretz <peretzman@gmail.com>
2017-08-24 12:26:21 -0500

    Added template to render as well as url_for link generation.

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -14,15 +14,9 @@ session = DBsession()
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
-    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
-
-    output = ''
-    for i in items:
-        output += i.name + '</br>'
-        output += i.price + '</br>'
-        output += i.description + '</br>'
-        output += '</br></br>'
-    return output
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
 
 @app.route('/restaurants/<int:restaurant_id>/new/')
